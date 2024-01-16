849b88da01db757b57db9381a3ade7bede2b9588
seed-yoo <lukdown10@naver.com>
2024-01-16 14:51:46 +0900

    연습문제 - 출력만 수정하면 됨

--- src/com/javaex/ex04/PhoneBookApp.java
+++ src/com/javaex/ex04/PhoneBookApp.java
@@ -1,22 +1,68 @@
 package com.javaex.ex04;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Scanner;
 
 public class PhoneBookApp {
 
 	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		String str;
+		
+		Reader fr = new FileReader("C:\\javaStudy\\workspace\\chapter05\\src\\com\\javaex\\ex03\\PhoneDB_원본.txt");
+		BufferedReader br = new BufferedReader(fr);
 		
 		// ※ 주의 - 쓰기를 사용하면 원본 파일이 다 없어짐
+		Writer out = new FileWriter("C:\\javaStudy\\PhoneDB_원본_inset.txt");
+		BufferedWriter bw = new BufferedWriter(out);
 		
 		// 파일에서 읽은 내용을 리스트로 관리
-		// (이름, 핸드폰, 회사)	--> Person
+		// (이름, 핸드폰, 회사)	--> 리스트 이름 : Person
+		
+		//System.out.println("정보를 입력하세요.");
+		String info = "유재석,010-3333-5555,비트";
+		
+		
+		while (true) {
+			// 파일을 1줄씩 읽는다.
+			str = br.readLine();
+			
+			// null 이면 반복문 끝.
+			if(str == null) {
+				bw.write(info);
+				break;
+			}
+			bw.write(str);
+			bw.newLine();
+			
+			String[] personInfo = str.split(",");
+			
+			String name = personInfo[0];
+			String hp = personInfo[1];
+			String company = personInfo[2];
+			
+			// 출력
+			System.out.println("이름: " + name );
+			System.out.println("핸드폰: " + hp );
+			System.out.println("회사: " + company );
+			System.out.println("");
+			
+		}
+		
 		
 		// 자신의 정보 Person 리스트에 추가하고
 		
 		// 리스트 전체 내용을 PhoneDb.txt에 씀
 		
-		
-		
+		br.close();
+		bw.close();
+		sc.close();
 	}
 
 }
