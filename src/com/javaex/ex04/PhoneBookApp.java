7659bc40e98d752ed2d3ff2cde8becb7e5f3196a
seed-yoo <lukdown10@naver.com>
2024-01-16 13:08:21 +0900

    연습문제(수정 전)

---
+++ src/com/javaex/ex04/PhoneBookApp.java
@@ -0,0 +1,22 @@
+package com.javaex.ex04;
+
+import java.io.IOException;
+
+public class PhoneBookApp {
+
+	public static void main(String[] args) throws IOException{
+		
+		// ※ 주의 - 쓰기를 사용하면 원본 파일이 다 없어짐
+		
+		// 파일에서 읽은 내용을 리스트로 관리
+		// (이름, 핸드폰, 회사)	--> Person
+		
+		// 자신의 정보 Person 리스트에 추가하고
+		
+		// 리스트 전체 내용을 PhoneDb.txt에 씀
+		
+		
+		
+	}
+
+}
